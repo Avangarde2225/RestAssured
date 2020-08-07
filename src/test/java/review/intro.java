ed549e17257dd5a33290f5964542db3b9cffc6ce
Serdar <serdar2225@gmail.com>
2020-08-07 05:57:22 -0400

    some more methods

--- src/test/java/review/intro.java
+++ src/test/java/review/intro.java
@@ -1,5 +1,6 @@
 package review;
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -13,5 +14,53 @@ public class intro {
                 get("http://zippopotam.us/us/90210").
                 then().
                 statusCode(200);
+    }
+
+    @Test
+    public void contentTypeTest() {
+        given().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().
+                contentType(ContentType.JSON);
+    }
+    @Test
+    public void logRequestAndResponseDetails() {
+        given().
+                log().all().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                log().body();
+    }
+
+    @Test
+    public void checkStateInResponseBody() {
+        given().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().
+                body("places[0].state",equalTo("California"));
+
+    }
+
+    @Test
+    public void checkListHasItem(){
+        given().
+                when().
+                get("http://api.zippopotam.us/tr/34295").
+                then().
+                body("places.'place name'",hasItem("Kartaltepe Mah."));
+    }
+    @Test
+    public void checkNumberOfPlaceNamesInResponseBody() {
+        given().
+                when().
+                get("http://zippopotam.us/tr/34840").
+                then().
+                assertThat().
+                body("places.'place name'", hasSize(2));
     }
 }
