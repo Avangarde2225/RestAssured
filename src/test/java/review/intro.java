5852e94df5b594c4275310d4ece69421c0a51a53
Serdar <serdar2225@gmail.com>
2020-08-07 06:15:52 -0400

    defining  a path

--- src/test/java/review/intro.java
+++ src/test/java/review/intro.java
@@ -1,17 +1,25 @@
 package review;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class intro {
+
+    @BeforeClass
+    public void init(){
+        RestAssured.baseURI = "http://api.zippopotam.us";
+    }
+
     @Test
     public void statusCodeTest() {
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("/us/90210").
                 then().
                 statusCode(200);
     }
@@ -20,7 +28,7 @@ public class intro {
     public void contentTypeTest() {
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("/us/90210").
                 then().
                 assertThat().
                 contentType(ContentType.JSON);
@@ -30,7 +38,7 @@ public class intro {
         given().
                 log().all().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("/us/90210").
                 then().
                 log().body();
     }
@@ -39,7 +47,7 @@ public class intro {
     public void checkStateInResponseBody() {
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get("/us/90210").
                 then().
                 assertThat().
                 body("places[0].state",equalTo("California"));
@@ -50,7 +58,7 @@ public class intro {
     public void checkListHasItem(){
         given().
                 when().
-                get("http://api.zippopotam.us/tr/34295").
+                get("/tr/34295").
                 then().
                 body("places.'place name'",hasItem("Kartaltepe Mah."));
     }
@@ -58,9 +66,33 @@ public class intro {
     public void checkNumberOfPlaceNamesInResponseBody() {
         given().
                 when().
-                get("http://zippopotam.us/tr/34840").
+                get("/tr/34840").
                 then().
                 assertThat().
                 body("places.'place name'", hasSize(2));
+    }
+    @Test
+    public void combiningTest() {
+        given().
+                when().
+                get("/us/90210").
+                then().
+                statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("places[0].state", equalTo("California"))
+                .body("places.'place name'", hasItem("Beverly Hills"))
+                .body("places.'place name'", hasSize(1));
+    }
+    @Test
+    public void pathParametersTest() {
+        given().
+                log().uri().
+                pathParam("countryCode", "us").pathParam("zipCode", "90210").
+                when().
+                get("/{countryCode}/{zipCode}").
+                then().
+                log().all().
+                assertThat().
+                body("places[0].'place name'", equalTo("Beverly Hills"));
     }
 }
